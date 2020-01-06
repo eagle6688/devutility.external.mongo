d7e112504cf4f439c660e4324691ac18da48f4da
Aldwin Su <fangyansu@163.com>
2020-01-06 19:33:13 +0800

    Add objectToQuery methods in MongoUtils.java

--- src/main/java/devutility/external/mongo/MongoUtils.java
+++ src/main/java/devutility/external/mongo/MongoUtils.java
@@ -1,9 +1,12 @@
 package devutility.external.mongo;
 
 import java.lang.reflect.Field;
+import java.util.Collection;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 
 import devutility.internal.lang.ClassUtils;
@@ -82,5 +85,51 @@ public class MongoUtils {
 	 */
 	public static Update objectToUpdate(Object entity, List<EntityField> entityFields) throws ReflectiveOperationException {
 		return objectToUpdate(entity, entityFields, true);
+	}
+
+	/**
+	 * Convert entity to Query object.
+	 * @param entity MongoDB entity.
+	 * @param entityFields EntityField objects.
+	 * @param containNullValue Whether contain field with null value in entity? The result does not contain fileds with null
+	 *            value if set containNullValue=false.
+	 * @return Query
+	 * @throws ReflectiveOperationException
+	 */
+	public static Query objectToQuery(Object entity, List<EntityField> entityFields, boolean containNullValue) throws ReflectiveOperationException {
+		if (entityFields == null) {
+			entityFields = ClassUtils.getEntityFields(entity.getClass());
+		}
+
+		Query query = new Query();
+
+		for (EntityField entityField : entityFields) {
+			Object fieldValue = entityField.getValue(entity);
+
+			if (!containNullValue && fieldValue == null) {
+				continue;
+			}
+
+			String fieldName = getFieldName(entityField.getField());
+
+			if (Collection.class.isAssignableFrom(fieldValue.getClass()) || fieldValue.getClass().isArray()) {
+				query.addCriteria(Criteria.where(fieldName).in(fieldValue));
+			} else {
+				query.addCriteria(Criteria.where(fieldName).is(fieldValue));
+			}
+		}
+
+		return query;
+	}
+
+	/**
+	 * Convert entity to Query object.
+	 * @param entity MongoDB entity.
+	 * @param entityFields EntityField objects.
+	 * @return Query
+	 * @throws ReflectiveOperationException
+	 */
+	public static Query objectToQuery(Object entity, List<EntityField> entityFields) throws ReflectiveOperationException {
+		return objectToQuery(entity, entityFields, true);
 	}
 }
